7853ac1678d1eec657ceaa4c6b17b50e1c34cf31
thouxzzh <2k21cse167@gmail.com>
2025-04-02 10:37:12 +0530

    Json Report

--- CucuSauceDemo/src/test/java/runnerfile/runnerClass.java
+++ CucuSauceDemo/src/test/java/runnerfile/runnerClass.java
@@ -1,7 +1,8 @@
 package runnerfile;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports/Cucumber.html"},features="src/test/resources/com/sauceDemo/sauceDemo.feature",glue="stepDefinitions")
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports/Cucumber.html",
+		                              "json:target/cucumber-reports/Cucumber.json"},features="src/test/resources/com/sauceDemo/sauceDemo.feature",glue="stepDefinitions")
 public class runnerClass extends  AbstractTestNGCucumberTests{
 
 }
